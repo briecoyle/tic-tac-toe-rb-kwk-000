c83a549648e099da3551cc99c08d4fde88c65262
Brie Coyle <brie.coyle@gmail.com>
2016-07-25 02:07:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,8 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
+  elsif draw?(board)
+    puts "Cats Game!"
   else
     turn(board)
   end
